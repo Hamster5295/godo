e5b5d8325917c4f3d5b258c548ea83f06f6a1e13
Hamster5295 <hamster5295@163.com>
2024-01-05 23:53:37 +0800

    Adding list command

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,19 @@
-pub fn get_file_name(tag: &String, mono: &bool) -> String {
+use console::style;
+use std::fs;
+
+pub const INSTALL_PATH: &str = "downloads";
+
+#[macro_export]
+macro_rules! err {
+    ($title:tt, $content:expr) => {
+        panic!("{}\n{}", style($title).red().bold(), style($content).dim())
+    };
+    ($title:tt) => {
+        panic!("{}", style($title).red().bold())
+    };
+}
+
+pub fn get_dir_name(tag: &String, mono: &bool) -> String {
     let mut result = format!("Godot_{}", tag);
     if *mono {
         result += "_mono";
@@ -12,4 +27,23 @@ pub fn get_version_name(tag: &String, mono: &bool) -> String {
         result += " mono";
     }
     result
+}
+
+pub fn create_install_path() {
+    fs::create_dir_all(INSTALL_PATH)
+        .unwrap_or_else(|err| err!("Unable to create install directory: ", err.to_string()));
+}
+
+pub fn get_installed_dirs() -> Vec<String> {
+    create_install_path();
+
+    let mut installs = vec![];
+    for dir_result in fs::read_dir(INSTALL_PATH).unwrap() {
+        let dir = dir_result.unwrap();
+        let path = dir.path();
+        if path.is_dir() {
+            installs.push(dir.file_name().to_str().unwrap().to_owned());
+        }
+    }
+    installs
 }
