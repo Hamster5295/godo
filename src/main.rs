5981cba5229cf1065866dadc27b7462a8bdfe144
Hamster5295 <hamster5295@163.com>
2024-01-05 23:20:47 +0800

    renaming utils.rs to remote.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod procedure;
+mod remote;
 mod utils;
 
 use std::fs;
@@ -6,8 +7,8 @@ use std::fs;
 use clap::{Parser, Subcommand};
 use console::Style;
 use dialoguer::Confirm;
+use remote::get_download_info;
 use reqwest::Client;
-use utils::get_download_info;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -56,17 +57,13 @@ async fn main() {
             let red = Style::new().red().bold();
             let green = Style::new().green().bold();
 
-            match get_download_info(&client, version_str, mono.to_owned()).await {
-                Some((file, url)) => {
+            match get_download_info(&client, version_str, *mono).await {
+                Some((tag, url)) => {
                     let proc = &mut procedure::new(4);
+                    let cur_version = utils::get_version_name(&tag, mono);
+
+                    // Confirm before download
                     proc.next("Please confirm your installation:".to_string());
-                    let version_name = file.trim();
-                    let cur_version = format!(
-                        "{} {} {}",
-                        "Godot",
-                        &version_name,
-                        if *mono { "mono" } else { "" }
-                    );
                     println!("\t> {} <", cyan.apply_to(&cur_version));
                     if Confirm::new()
                         .with_prompt("Do you want to proceed?")
@@ -76,29 +73,36 @@ async fn main() {
                         .interact()
                         .unwrap()
                     {
+                        // Start download
                         proc.next(format!("{}", yellow.apply_to("Downloading...")));
-                        let path = utils::download(
+                        let path = remote::download(
                             &client,
-                            format!("Godot_{}{}", &file, if *mono { "_mono" } else { "" }),
+                            utils::get_file_name(&tag, mono),
                             url,
                         )
                         .await;
                         proc.finish("Download Completed!".to_string());
+
+                        // Unzip
                         proc.next(format!("{}", yellow.apply_to("Unzipping...")));
-                        utils::unzip(&path);
+                        remote::unzip(&path);
                         proc.finish("Unzipped!".to_string());
+
+                        // Remove the original zipped file
                         proc.next(format!("{}", yellow.apply_to("Clearing cache...")));
                         fs::remove_file(&path).unwrap();
                         proc.finish("Cleared!".to_string());
+
+                        // Finished!
                         println!(
                             "{} is now {}.",
                             cyan.apply_to(&cur_version),
                             green.apply_to("READY")
                         );
                         println!(
-                            "Use {} {} to begin.",
+                            "Use {} {} to start.",
                             yellow.apply_to("godo run"),
-                            green.apply_to(&file)
+                            green.apply_to(&tag)
                         );
                     } else {
                         println!("{}", red.apply_to("Installation aborted"))
@@ -115,7 +119,7 @@ async fn main() {
         }
         Command::Available { prerelease } => {
             let client = Client::new();
-            utils::list_avail(&client, *prerelease).await;
+            remote::list_avail(&client, *prerelease).await;
         }
     }
 }
