434d2f2448dcd5c14eb9745eb06b7ee347e5c459
Hamster5295 <hamster5295@163.com>
2024-01-06 14:05:40 +0800

    Adding search function for installed versions

--- src/version.rs
+++ src/version.rs
@@ -1,6 +1,4 @@
-use std::io;
-
-use io::Error;
+use std::cmp::Ordering;
 
 pub struct Version {
     tag: String,
@@ -11,21 +9,56 @@ pub fn new(tag: String, mono: bool) -> Version {
     Version { tag, mono }
 }
 
-pub fn parse(name: String) -> Result<Version, Error> {
+pub fn parse(name: String) -> Option<Version> {
     let results: Vec<&str> = name.split(&[' ', '_']).collect();
     if results.len() < 2 {
-        Err(Error::new(
-            std::io::ErrorKind::InvalidInput,
-            format!(
-                "Wrong format! Require Godot_[version]_[mono] or Godot [version] [mono], found {}",
-                name
-            ),
-        ))
+        None
     } else {
-        Ok(Version {
+        Some(Version {
             tag: results[1].to_string(),
             mono: results.len() >= 3,
         })
+    }
+}
+
+pub fn compare(tag1: String, tag2: String) -> Ordering {
+    let mut v1: Vec<&str> = tag1.split('.').collect();
+    while v1.len() < 3 {
+        v1.push("0");
+    }
+    let mut v2: Vec<&str> = tag2.split('.').collect();
+    while v2.len() < 3 {
+        v2.push("0");
+    }
+
+    let mut cmp = v1[0].cmp(v2[0]);
+    if cmp != Ordering::Equal {
+        return cmp;
+    }
+    cmp = v1[1].cmp(v2[1]);
+    if cmp != Ordering::Equal {
+        return cmp;
+    }
+    cmp = v1[2].cmp(v2[2]);
+    if cmp != Ordering::Equal {
+        return cmp;
+    }
+    Ordering::Equal
+}
+
+pub fn compare_opt(ver1: &Option<Version>, ver2: &Option<Version>) -> Ordering {
+    if let Some(v1) = ver1 {
+        if let Some(v2) = ver2 {
+            compare(v1.tag(), v2.tag())
+        } else {
+            Ordering::Greater
+        }
+    } else {
+        if let Some(_v2) = ver2 {
+            Ordering::Less
+        }else{
+            Ordering::Equal
+        }
     }
 }
 
